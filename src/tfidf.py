ac2bf1acadb0c2f2e7afc0f496df90403643f238
jeshrz <huangruizhe@live.cn>
2020-03-19 14:48:35 +0800

    remove redundant codes

--- src/tfidf.py
+++ src/tfidf.py
@@ -2,12 +2,10 @@ import jieba
 from gensim.similarities import SparseMatrixSimilarity
 from gensim.corpora import Dictionary
 from gensim.models import TfidfModel
-import warnings
-warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+
 import numpy as np
 import xlrd
 from copy import deepcopy as dco
-import xlsxwriter
 
 
 def similar_result(sentence, key):
@@ -33,8 +31,9 @@ def similar_result(sentence, key):
         result.append(s)
     return result
 
-message=np.load('message.npy').item()
-classifications=np.load('classifications.npy').item()
+
+message = np.load('message.npy').item()
+classifications = np.load('classifications.npy').item()
 
 template_cnt = {
     '城乡建设': 0,
@@ -51,10 +50,8 @@ template_cnt = {
     '卫生计生': 0,
     '政法': 0,
     '教育文体': 0,
-    '劳动和社会保障': 0}
-
-# print(message)
-# print(classifications)
+    '劳动和社会保障': 0,
+}
 
 keys = list(message.values())
 ans = list(classifications)
@@ -62,35 +59,30 @@ ans = list(classifications)
 sent = list(classifications.values())
 list_mine = []
 
-with open('mine.txt','w') as f:
-    for k in keys:
-        grade = similar_result(sent,k)
-        list_mine.append(ans[np.argsort(grade)[-1]])
+for k in keys:
+    grade = similar_result(sent, k)
+    list_mine.append(ans[np.argsort(grade)[-1]])
 
-path = '附件2.xlsx'
-ExcelFile1=xlrd.open_workbook(path)
-sheet1=ExcelFile1.sheet_by_index(0)
-list_ans=[]
+path = 'data/messages.xlsx'
+ExcelFile1 = xlrd.open_workbook(path)
+sheet1 = ExcelFile1.sheet_by_index(0)
+list_ans = []
 # workbook3 = xlsxwriter.Workbook('wrong.xlsx')  # 创建一个excel文件
 # worksheet3 = workbook3.add_worksheet(u'sheet1')  # 在文件中创建一个名为TEST的sheet,不加名字默认为sheet1
 for i in range(1, sheet1.nrows):
-
-        text = sheet1.cell(i, 5).value.encode('utf-8').decode('utf-8-sig')
-        list_ans.append(text)
-#         if list_mine[i-1] != text:
-#             worksheet3.write(i, 0, list_mine[i-1])
+    text = sheet1.cell(i, 5).value.encode('utf-8').decode('utf-8-sig')
+    list_ans.append(text)
 # workbook3.close()
 
 
-
-#evalue
+# evalue
 true_cnt = dco(template_cnt)
 ans_cnt = dco(template_cnt)
 mine_cnt = dco(template_cnt)
 
-
 if len(list_ans) != len(list_mine):
     print("error")
+    exit(0)
 
 for pos in range(0, len(list_ans)):
     ans_cnt[list_ans[pos]] += 1
@@ -110,7 +102,6 @@ for item in keys:
             pi = true_cnt[item] / mine_cnt[item]
         ri = true_cnt[item] / ans_cnt[item]
         ans += 2 * pi * ri / (pi + ri)
-        print(item,':',2 * pi * ri / (pi + ri))
+        print(item, ':', 2 * pi * ri / (pi + ri))
 ans /= num
-print("总分:",ans)
-
+print("总分:", ans)
