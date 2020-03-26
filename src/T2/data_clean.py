ae6ba60ee1b9bb254f7031a547ed146c92f1a6ef
jeshrz <huangruizhe@live.cn>
2020-03-26 15:53:29 +0800

    apply stop word

--- src/T2/data_clean.py
+++ src/T2/data_clean.py
@@ -1,35 +1,12 @@
 import jieba
+import jieba.analyse
 import xlrd
 
 jieba.load_userdict("data/address_words.txt")
-# jieba.load_userdict("data/sogou_dict.txt")
-strs = ["A市经济学院体育学院变相强制实习", "在A市人才app上申请购房补贴为什么通不过", "希望西地省把抗癌药品纳入医保范围", "A5区劳动东路魅力之城小区底层餐馆油烟扰民"]
-# for str in strs:
-#     seg_list = jieba.cut(str)
-#     print("/".join(seg_list))
-# jieba.add_word("经济学院", freq=20000, tag=None)
-# jieba.suggest_freq('A5区', True)
-for str in strs:
-    # seg_list = jieba.cut(str, use_paddle=True, HMM=False)  # 使用paddle模式
-    seg_list = jieba.cut(str, HMM=True)
-    print("Paddle Mode: " + '/'.join(list(seg_list)))
-
-# import string
-#
-# base = ["县", "市", "区", "省", "路", "镇", "小区"]
-#
-# fo = open("data/address_words.txt", "w")
-#
-# for word in string.ascii_uppercase:
-#     for k in base:
-#         fo.write(word + k + " 10" + '\n')
-#     for j in range(1, 21):
-#         fo.write(word + str(j) + " 10" + '\n')
-#         for k in base:
-#             fo.write(word + str(j) + k + " 10" + '\n')
-
+jieba.load_userdict("data/sogou_dict.txt")
+jieba.analyse.set_stop_words("data/stop_words.txt")
 sheet = xlrd.open_workbook("data/messages2.xlsx").sheet_by_index(0)
 for i in range(1, sheet.nrows):
     msg = sheet.cell(i, 2).value
-    seg_list = jieba.cut(msg, HMM=True)
+    seg_list = [key_word[0] for key_word in jieba.analyse.extract_tags(msg, 50, withWeight=True)]
     print("jieba Mode: " + '/'.join(list(seg_list)))
