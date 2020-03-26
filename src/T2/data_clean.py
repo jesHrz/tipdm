557cbe51dc95b6f4e08fafacc41c96207aab3fff
jeshrz <huangruizhe@live.cn>
2020-03-26 15:45:23 +0800

    add sogou cell dirc

--- src/T2/data_clean.py
+++ src/T2/data_clean.py
@@ -5,11 +5,12 @@ jieba.initialize()
 jieba.set_dictionary("data/address_words.txt")
 jieba.enable_paddle()  # 启动paddle模式。 0.40版之后开始支持，早期版本不支持
 jieba.load_userdict("data/address_words.txt")
+# jieba.load_userdict("data/sogou_dict.txt")
 strs = ["A市经济学院体育学院变相强制实习", "在A市人才app上申请购房补贴为什么通不过", "希望西地省把抗癌药品纳入医保范围", "A5区劳动东路魅力之城小区底层餐馆油烟扰民"]
 # for str in strs:
 #     seg_list = jieba.cut(str)
 #     print("/".join(seg_list))
-# jieba.add_word("A5区", freq = 20000, tag = None)
+# jieba.add_word("经济学院", freq=20000, tag=None)
 # jieba.suggest_freq('A5区', True)
 for str in strs:
     # seg_list = jieba.cut(str, use_paddle=True, HMM=False)  # 使用paddle模式
